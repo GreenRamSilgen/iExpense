cde8fac1acd6e4b890d77e46654f8b1a6735619d
GreenRamSilgen <kiranshrs@yahoo.com>
2025-03-14 15:11:01 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kiran Shrestha on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
